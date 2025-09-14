50a1f15fa4382003c4cb42e5139f93b8350ac805
Maria Lykotrafiti <lyk.maria@yahoo.gr>
2025-09-14 16:12:50 +0300

    Step 11: UI polish (Bootstrap CDN, navbar, home hero/cards, friendly error page, tempdata alerts)

---
+++ src/PetAdopt.Web/Pages/Shared/_TempDataAlert.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PetAdopt.Web.Pages.Shared
+{
+    public class _TempDataAlertModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- src/PetAdopt.Web/Program.cs
+++ src/PetAdopt.Web/Program.cs
@@ -37,6 +37,8 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+app.UseStatusCodePagesWithReExecute("/Error", "?code={0}");
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
